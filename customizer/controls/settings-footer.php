abf22d111692c7046ca0ed170bd6b4ba0ff024e1
test176 <60094687+test176@users.noreply.github.com>
2020-01-20 22:55:39 +0600

    test01

---
+++ customizer/controls/settings-footer.php
@@ -0,0 +1,255 @@
+<?php
+
+
+//============================FOOTER SECTION=================================
+
+//Scroll To Top Button
+$wp_customize->add_setting('complete[totop_id]', array(
+	'type' => 'option',
+	'default' => '1',
+	'sanitize_callback' => 'complete_sanitize_checkbox',
+	'transport' => 'postMessage',
+) );
+			$wp_customize->add_control( new complete_Controls_Toggle_Control( $wp_customize, 'totop_id', array(
+				'label' => __('Scroll To Top Button','complete'),
+				'description' => __( 'Turn On/Off The button that appears on bottom right when you scroll down to pages.', 'complete' ),
+				'section' => 'footercolors_section',
+				'settings' => 'complete[totop_id]',
+			)) );
+
+
+// Footer Background Color
+$wp_customize->add_setting( 'complete[footer_color_id]', array(
+	'type' => 'option',
+	'default' => '#151515',
+	'sanitize_callback' => 'sanitize_hex_color',
+	'transport' => 'postMessage',
+) );
+
+			$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_color_id', array(
+				'label' => __('Footer Background Color','complete'),
+				'section' => 'footercolors_section',
+				'settings' => 'complete[footer_color_id]',
+			) ) );
+
+// Footer Background Image
+	$wp_customize->add_setting( 'complete[footer_bg_image]',array( 
+		'type' => 'option',
+		'default' => ''.get_template_directory_uri().'/images/footer-bg.jpg',
+		'sanitize_callback' => 'esc_url_raw',
+		)
+	);
+	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'footer_bg_image',array(
+			'label'       => __( 'Footer Background Image', 'complete' ),
+			'section'     => 'footercolors_section',
+			'settings'    => 'complete[footer_bg_image]'
+				)
+			)
+	);
+
+//FOOTER Widget Text Color
+$wp_customize->add_setting( 'complete[footwdgtxt_color_id]', array(
+	'type' => 'option',
+	'default' => '#a6a6a6',
+	'sanitize_callback' => 'sanitize_hex_color',
+	'transport' => 'postMessage',
+) );
+
+			$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footwdgtxt_color_id', array(
+				'label' => __('Footer Text Color','complete'),
+				'section' => 'footercolors_section',
+				'settings' => 'complete[footwdgtxt_color_id]',
+			) ) );
+
+//FOOTER Widget Title Color
+$wp_customize->add_setting( 'complete[footer_title_color]', array(
+	'type' => 'option',
+	'default' => '#ffffff',
+	'sanitize_callback' => 'sanitize_hex_color',
+	'transport' => 'postMessage',
+) );
+
+			$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_title_color', array(
+				'label' => __('Footer Title Color','complete'),
+				'section' => 'footercolors_section',
+				'settings' => 'complete[footer_title_color]',
+			) ) );
+
+//FOOTER Widget Title Border Color
+$wp_customize->add_setting( 'complete[footer_title_border_color]', array(
+	'type' => 'option',
+	'default' => '#ffb000',
+	'sanitize_callback' => 'sanitize_hex_color',
+	'transport' => 'postMessage',
+) );
+
+			$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_title_border_color', array(
+				'label' => __('Footer Title Color','complete'),
+				'section' => 'footercolors_section',
+				'settings' => 'complete[footer_title_border_color]',
+			) ) );
+
+$wp_customize->add_setting( 'complete[footer_menu_color]', array(
+	'type' => 'option',
+	'default' => '#a6a6a6',
+	'sanitize_callback' => 'sanitize_hex_color',
+	'transport' => 'postMessage',
+) );
+
+			$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_menu_color', array(
+				'label' => __('Footer Menu Color','complete'),
+				'section' => 'footercolors_section',
+				'settings' => 'complete[footer_menu_color]',
+			) ) );
+			
+$wp_customize->add_setting( 'complete[footer_menu_hover_color]', array(
+	'type' => 'option',
+	'default' => '#ffffff',
+	'sanitize_callback' => 'sanitize_hex_color',
+	'transport' => 'postMessage',
+) );
+
+			$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_menu_hover_color', array(
+				'label' => __('Footer Menu Hover Color','complete'),
+				'section' => 'footercolors_section',
+				'settings' => 'complete[footer_menu_hover_color]',
+			) ) );
+			
+//FOOTER LAYOUT SELECT
+$wp_customize->add_setting('complete[foot_layout_id]', array(
+		'type' => 'option',
+        'default' => '4',
+		'sanitize_callback' => 'complete_sanitize_choices',
+) );
+ 
+			$wp_customize->add_control( new complete_Control_Radio_Image( $wp_customize, 'foot_layout_id', array(
+					'type' => 'radio-image',
+					'label' => __('Footer Layout','complete'),
+					'section' => 'footercolors_section',
+					'settings' => 'complete[foot_layout_id]',
+					'choices' => array(
+						'4' => array( 'url' => get_template_directory_uri().'/assets/images/foot-4-col.jpg', 'label' => 'Layout 4' ),
+					),
+			) ));			
+			
+//----------------------Footer Columns 1----------------------------------
+	$wp_customize->add_setting('complete[foot_cols1_title]', array(
+		'type' => 'option',
+		'default'	=> __('SKT Ele Ebook Lite','complete'),
+		'sanitize_callback' => 'wp_kses_post',
+		'transport' => 'postMessage',
+	) );
+	$wp_customize->add_control(	new WP_Customize_Text_Control( $wp_customize, 'foot_cols1_title', array( 
+		'type' => 'text',
+		'label'	=> __('Columns 1 Title','complete'),
+		'section' => 'footer_columns_section',
+		'settings' => 'complete[foot_cols1_title]',
+	)) );	
+	
+$wp_customize->add_setting('complete[foot_cols1_content]', array(
+	'type' => 'option',
+	'default' => 'Pellentesque placerat urna eu iaculis dictum. Donec bibendum pellentesq risus, commodo dapibus eros maximus quis. Curabitur at finibus nulla, eget aliqua quam bibendum pellentesq risus.[space height="50"][social_area]
+	[social icon="facebook" link="#"]
+	[social icon="twitter" link="#"]
+	[social icon="instagram" link="#"]
+	[social icon="linkedin" link="#"]
+	[social icon="pinterest" link="#"] 
+[/social_area]',
+	'sanitize_callback' => 'wp_kses_post',
+	'transport' => 'postMessage',
+) );
+			$wp_customize->add_control(	new complete_Editor_Control( $wp_customize, 'foot_cols1_content', array( 
+				'type' => 'editor',
+				'label' => __('Columns 1 Content','complete'), 
+				'section' => 'footer_columns_section',
+				'settings' => 'complete[foot_cols1_content]',
+			)) );	
+ 	 
+//----------------------Footer Columns 1----------------------------------		
+
+//----------------------Footer Columns 2----------------------------------
+	$wp_customize->add_setting('complete[foot_cols2_title]', array(
+		'type' => 'option',
+		'default'	=> __('OUR SERVICES','complete'),
+		'sanitize_callback' => 'wp_kses_post',
+		'transport' => 'postMessage',
+	) );
+	$wp_customize->add_control(	new WP_Customize_Text_Control( $wp_customize, 'foot_cols2_title', array( 
+		'type' => 'text',
+		'label'	=> __('Columns 2 Title','complete'),
+		'section' => 'footer_columns_section',
+		'settings' => 'complete[foot_cols2_title]',
+	)) );	
+	
+$wp_customize->add_setting('complete[foot_cols2_content]', array(
+	'type' => 'option',
+	'default' => '[footermenu menu="services"]',
+	'sanitize_callback' => 'wp_kses_post',
+	'transport' => 'postMessage',
+) );
+			$wp_customize->add_control(	new complete_Editor_Control( $wp_customize, 'foot_cols2_content', array( 
+				'type' => 'editor',
+				'label' => __('Columns 2 Content','complete'), 
+				'section' => 'footer_columns_section',
+				'settings' => 'complete[foot_cols2_content]',
+			)) );	
+ 	 
+//----------------------Footer Columns 2----------------------------------	
+
+//----------------------Footer Columns 3----------------------------------
+	$wp_customize->add_setting('complete[foot_cols3_title]', array(
+		'type' => 'option',
+		'default'	=> __('CONTACT US','complete'),
+		'sanitize_callback' => 'wp_kses_post',
+		'transport' => 'postMessage',
+	) );
+	$wp_customize->add_control(	new WP_Customize_Text_Control( $wp_customize, 'foot_cols3_title', array( 
+		'type' => 'text',
+		'label'	=> __('Columns 3 Title','complete'),
+		'section' => 'footer_columns_section',
+		'settings' => 'complete[foot_cols3_title]',
+	)) );	
+	
+$wp_customize->add_setting('complete[foot_cols3_content]', array(
+	'type' => 'option',
+	'default' => 'Street 238,52 tempor <br> Donec ultricies mattis nulla, suscipit risus tristique utmattis nulla.[space height="40"]<p style="margin-bottom: 10px;"><span style="color: #fff; font-weight: bold; margin-right: 30px;">Phone:</span> 1.800.555.6789</p><p style="margin-bottom: 10px;"><span style="color: #fff; font-weight: bold; margin-right: 34px;">E-mail:</span>info@sktthemes.org</a></p><p style="margin-bottom: 10px;"><span style="color: #fff; font-weight: bold; margin-right: 23px;">Website:</span>https://sktthemes.org</a></p>',
+	'sanitize_callback' => 'wp_kses_post',
+	'transport' => 'postMessage',
+) );
+			$wp_customize->add_control(	new complete_Editor_Control( $wp_customize, 'foot_cols3_content', array( 
+				'type' => 'editor',
+				'label' => __('Columns 3 Content','complete'), 
+				'section' => 'footer_columns_section',
+				'settings' => 'complete[foot_cols3_content]',
+			)) );	
+ 	 
+//----------------------Footer Columns 3----------------------------------	
+
+//----------------------Footer Columns 4----------------------------------
+	$wp_customize->add_setting('complete[foot_cols4_title]', array(
+		'type' => 'option',
+		'default'	=> __('LATEST POSTS','complete'),
+		'sanitize_callback' => 'wp_kses_post',
+		'transport' => 'postMessage',
+	) );
+	$wp_customize->add_control(	new WP_Customize_Text_Control( $wp_customize, 'foot_cols4_title', array( 
+		'type' => 'text',
+		'label'	=> __('Columns 4 Title','complete'),
+		'section' => 'footer_columns_section',
+		'settings' => 'complete[foot_cols4_title]',
+	)) );	
+	
+$wp_customize->add_setting('complete[foot_cols4_content]', array(
+	'type' => 'option',
+	'default' => '[footerposts show="6"]',
+	'sanitize_callback' => 'wp_kses_post',
+	'transport' => 'postMessage',
+) );
+			$wp_customize->add_control(	new complete_Editor_Control( $wp_customize, 'foot_cols4_content', array( 
+				'type' => 'editor',
+				'label' => __('Columns 4 Content','complete'), 
+				'section' => 'footer_columns_section',
+				'settings' => 'complete[foot_cols4_content]',
+			)) );	
+ 	 
+//----------------------Footer Columns 4----------------------------------	
